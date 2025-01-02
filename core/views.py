11f556a6ba266f9b41ce5075f8132255191f6ce2
yukito.takeuchi <iyggf66974@yahoo.ne.jp>
2025-01-02 21:12:00 +0900

    BS ok

--- core/views.py
+++ core/views.py
@@ -1,20 +1,27 @@
 from django.shortcuts import redirect, render
 
 from core.forms import CreateForm
+from core.models import Post
 
 
 def index(request):
-    return render(request,'core/index.html')
+    posts=Post.objects.all()
+    
+    context={
+        'posts': posts,
+    }
+    return render(request,'core/index.html',context)
 
 def create(request):
     if request.method == 'POST':
         form=CreateForm(request.POST)
-        if form.is_valide():
+        if form.is_valid():
             post=form.save(commit=False)
             post.save()
             return redirect('index')
     else:
         form=CreateForm()
+        
     context={
         'form': form,
     }
