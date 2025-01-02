05af8b16efcc430b7418434dc4000552de11003a
yukito.takeuchi <iyggf66974@yahoo.ne.jp>
2025-01-02 19:04:33 +0900

    register login ok

--- userauth/forms.py
+++ userauth/forms.py
@@ -1,7 +1,12 @@
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
 from django.contrib.auth.models import User
 
 class RegisterForm(UserCreationForm):
     class Meta:
         model=User
-        fields=['username','email','password']
+        fields=['username','email','password1','password2']
+        
+class LoginForm(AuthenticationForm):
+    class Meta:
+        model=User
+       
--- userauth/urls.py
+++ userauth/urls.py
@@ -5,4 +5,5 @@ from userauth import views
 
 urlpatterns = [
     path('register',views.register.as_view(),name='register'),
+    path('',views.login.as_view(),name='login'),
 ]
